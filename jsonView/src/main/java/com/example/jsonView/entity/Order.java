a6b78e5b6f1bd0a85daf9a7dd3eb0df6bddc2044
dirtyphonicsss@yandex.ru <dirtyphonicsss@yandex.ru>
2025-01-29 16:46:21 +0300

    lombok

--- jsonView/src/main/java/com/example/jsonView/entity/Order.java
+++ jsonView/src/main/java/com/example/jsonView/entity/Order.java
@@ -3,8 +3,11 @@ package com.example.jsonView.entity;
 import com.example.jsonView.dto.order.OrderSummary;
 import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@Data
+@NoArgsConstructor
 @Entity(name = "`order`")
 public class Order {
 
@@ -27,44 +30,4 @@ public class Order {
     @JsonView(OrderSummary.class)
     private String status;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getProduct() {
-        return product;
-    }
-
-    public void setProduct(String product) {
-        this.product = product;
-    }
-
-    public double getTotalAmount() {
-        return totalAmount;
-    }
-
-    public void setTotalAmount(double totalAmount) {
-        this.totalAmount = totalAmount;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
--- jsonView/src/main/java/com/example/jsonView/entity/User.java
+++ jsonView/src/main/java/com/example/jsonView/entity/User.java
@@ -4,10 +4,13 @@ import com.example.jsonView.dto.user.UserDetails;
 import com.example.jsonView.dto.user.UserSummary;
 import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
-
+@Data
 @Entity(name = "`user`")
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -31,38 +34,4 @@ public class User {
         this.email = email;
     }
 
-    public User() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<Order> getOrders() {
-        return orders;
-    }
-
-    public void setOrders(List<Order> orders) {
-        this.orders = orders;
-    }
 }
